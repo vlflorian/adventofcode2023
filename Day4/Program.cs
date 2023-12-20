062aafdd88feef896acaf0d26b837f27b6f460a5
Florian Van Lantschoot <florian.vanlantschoot@gmail.com>
2023-12-20 20:23:55 +0100

    Day 4 part 1

---
+++ Day4/Program.cs
@@ -0,0 +1,26 @@
+﻿var sampleInput = @"
+Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
+";  // winning numbers before |, lottery numbers after 
+
+// var lines = sampleInput.Split('\n').Where(x => !string.IsNullOrWhiteSpace(x));
+var lines = File.ReadAllText("input.txt").Split('\n').Where(x => !string.IsNullOrWhiteSpace(x));
+
+double totalScore = 0;
+foreach (var line in lines)
+{
+    var allNumbers = line.Split(':')[1].Replace("  ", " ");
+    var winningNumbers = allNumbers.Split(" | ")[0].Trim().Split(' ').Select(int.Parse);
+    var myNumbers = allNumbers.Split(" | ")[1].Trim().Split(' ').Select(int.Parse);
+    
+    var matchingNumbers = winningNumbers.Intersect(myNumbers);
+    var score = matchingNumbers.Any() ? Math.Pow(2, matchingNumbers.Count() - 1) : 0;
+    totalScore += score;
+    Console.WriteLine($"{string.Join(',', winningNumbers)}|{string.Join(',', myNumbers)} -> [{string.Join(',', matchingNumbers)}] -> score of {score}");
+}
+
+Console.WriteLine($"Total score: {totalScore}");
